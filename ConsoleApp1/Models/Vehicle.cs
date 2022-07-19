54d6483f0ae5111a1e29ce97ada2a4f6b6027ce0
Eduardo Borges <alemaobrk1@gmail.com>
2022-07-19 09:11:44 -0300

    create class vehicle

---
+++ ConsoleApp1/Models/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Vehicle
+    {
+        public int Chassis { get; set; }
+        public DateOnly ManufacturingDate { get; set; }
+        public string Name { get; set; }
+        public string LicensePlate { get; set; }
+        public double Value { get; set; }
+        public string Cpf { get; set; }
+        public string Color { get; set; }
+        public int Store { get; set; }
+
+        Random randNum = new Random();
+        public Vehicle (
+            DateOnly manufacturingDate,
+            string name, 
+            string licensePlate, 
+            double value, 
+            string cpf, 
+            string color
+            )
+        {
+            this.Chassis = randNum.Next();
+            this.ManufacturingDate = manufacturingDate;
+            this.Name = name;
+            this.LicensePlate = licensePlate;
+            this.Value = value;
+            this.Cpf = cpf;
+            this.Color = color;
+            this.Store = 0;
+        }
+        public void BuyVehicle (int value)
+        {
+            this.Store += value;
+        }
+
+        public void SellVehicle (int value)
+        {
+            this.Store -= value;
+        }
+
+        public void ListData ()
+        {
+            Console.WriteLine($"Modelo: {Name}");
+            Console.WriteLine($"Chassi: {Chassis}");
+            Console.WriteLine($"Placa: {LicensePlate}");
+            Console.WriteLine($"Valor: R$ {Value:F2}");
+            Console.WriteLine($"Estoque: {Store}");
+        }
+
+        public void SetColor (string color)
+        {
+            this.Color = color;
+        }
+
+        public void SetValue (int value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1.Models;
+
+
+var date = new DateOnly(2022, 01, 05);
+
+var firstVehicle = new Vehicle(date, "Honda", "NKC2864", 134000, "01101101101101", "White");
+
+firstVehicle.BuyVehicle(7);
+firstVehicle.ListData();
